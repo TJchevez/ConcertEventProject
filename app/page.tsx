009f154946e949f166d1ed852686bf393c6f921a
TJchevez <cheveztj@gmail.com>
2026-01-26 19:36:02 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    console.log("What type of a component am I");
+
+    return (
+        <div className="text-5xl underline">Welcome to Next.js!</div>
+    )
+}
+export default Home
